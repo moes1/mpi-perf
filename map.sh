bf075663eae976cec9b66b20a1199af01db245fd
Scott Moe <moes1@noreply.com>
2023-05-08 09:56:57 -0700

    updating map.sh

--- map.sh
+++ map.sh
@@ -2,15 +2,15 @@
  
  
 case $OMPI_COMM_WORLD_LOCAL_RANK in
-      0) core=0,23;    export UCX_NET_DEVICES=mlx5_ib0:1 ;;
-      1) core=0,23;    export UCX_NET_DEVICES=mlx5_ib1:1 ;;
-      2) core=24,47;    export UCX_NET_DEVICES=mlx5_ib2:1 ;;
-      3) core=24,47;    export UCX_NET_DEVICES=mlx5_ib6:1 ;;
+      0) core=0-47;    export UCX_NET_DEVICES=mlx5_ib0:1 ;;
+      1) core=0-47;    export UCX_NET_DEVICES=mlx5_ib1:1 ;;
+      2) core=0-47;    export UCX_NET_DEVICES=mlx5_ib2:1 ;;
+      3) core=0-47;    export UCX_NET_DEVICES=mlx5_ib3:1 ;;
 
-      4) core=48,71;    export UCX_NET_DEVICES=mlx5_ib3:1 ;;
-      5) core=48,71;    export UCX_NET_DEVICES=mlx5_ib4:1 ;;
-      6) core=72,95;    export UCX_NET_DEVICES=mlx5_ib5:1 ;;
-      7) core=72,95;    export UCX_NET_DEVICES=mlx5_ib7:1 ;;
+      4) core=48-95;    export UCX_NET_DEVICES=mlx5_ib4:1 ;;
+      5) core=48-95;    export UCX_NET_DEVICES=mlx5_ib5:1 ;;
+      6) core=48-95;    export UCX_NET_DEVICES=mlx5_ib6:1 ;;
+      7) core=48-95;    export UCX_NET_DEVICES=mlx5_ib7:1 ;;
 
 esac
  
